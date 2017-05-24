7ca4a111a469ad4aac230c006aa1f7c18a39ae63
bigrocs <bigrocs@qq.com>
2017-05-24 16:21:53 +0800

    增加侧栏切换按钮

--- src/components/Layout/Navbar.vue
+++ src/components/Layout/Navbar.vue
@@ -1,39 +1,44 @@
 <template>
   <div class="layout-header">
+    <span class="toggle" @click="toggleClick"><i class="fa fa-sliders"></i></span>
     <el-menu default-active="1" class="el-menu-navbar" mode="horizontal">
       <el-menu-item index="1"> <i class="fa fa-archive"></i> 系统</el-menu-item>
-      <el-submenu index="2">
-        <template slot="title">我的工作台</template>
-        <el-menu-item index="2-1">选项1</el-menu-item>
-        <el-menu-item index="2-2">选项2</el-menu-item>
-        <el-menu-item index="2-3">选项3</el-menu-item>
-      </el-submenu>
-      <el-submenu index="2">
-        <template slot="title">我的工作台</template>
-        <el-menu-item index="2-1">选项1</el-menu-item>
-        <el-menu-item index="2-2">选项2</el-menu-item>
-        <el-menu-item index="2-3">选项3</el-menu-item>
-      </el-submenu>
-      <el-submenu index="2">
-        <template slot="title">我的工作台</template>
-        <el-menu-item index="2-1">选项1</el-menu-item>
-        <el-menu-item index="2-2">选项2</el-menu-item>
-        <el-menu-item index="2-3">选项3</el-menu-item>
-      </el-submenu>
+      <el-menu-item index="2"> <i class="fa fa-archive"></i> CMS</el-menu-item>
+      <el-menu-item index="3"> <i class="fa fa-archive"></i> 用户</el-menu-item>
     </el-menu>
   </div>
 </template>
 <script>
   export default {
-    name: 'navbar'
+    name: 'navbar',
+    methods: {
+      toggleClick() {
+        console.log('test')
+      }
+    }
   }
 </script>
 <style lang="scss" scoped>
   .layout-header{
     background: #fff;
     box-shadow: 0 1px 1px rgba(0,0,0,.1);
+    display: -webkit-box;
+  }
+  .toggle{
+    display: -webkit-box;
+    margin: 15px;
+    font-size: 22px;
+    :hover{
+      color: #20a0ff;
+    }
+  }
+  .el-menu-item{
+    border-bottom: 2px solid transparent;
   }
   .el-menu-item.is-active{
+    border-bottom: 2px solid #20a0ff;
+  }
+  .el-menu-item:hover{
     border-bottom: 2px solid #20a0ff;
   }
   .el-menu{
