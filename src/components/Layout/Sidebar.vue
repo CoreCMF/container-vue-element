28ef7c9ebe187dc915987ea014293da19fd22822
bigrocs <bigrocs@qq.com>
2017-06-06 11:27:42 +0800

    侧栏 只保持一个子菜单的展开

--- src/components/Layout/Sidebar.vue
+++ src/components/Layout/Sidebar.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-menu default-active="1-4-1" theme="dark" class="sidebar-menu">
+  <el-menu :unique-opened="true" default-active="1-4-1" theme="dark" class="sidebar-menu">
       <el-submenu index="1">
         <template slot="title"><i class="el-icon-message"></i><span>Dashboard</span></template>
           <el-menu-item index="1-1">选项1</el-menu-item>
@@ -24,18 +24,18 @@
           <el-menu-item index="2-4-1">选项1</el-menu-item>
         </el-submenu>
       </el-submenu>
-      <el-submenu index="2">
+      <el-submenu index="3">
        <template slot="title"><i class="el-icon-message"></i><span>Dashboard</span></template>
         <el-menu-item-group title="分组一">
-          <el-menu-item index="2-1">选项1</el-menu-item>
-          <el-menu-item index="2-2">选项2</el-menu-item>
+          <el-menu-item index="3-1">选项1</el-menu-item>
+          <el-menu-item index="3-2">选项2</el-menu-item>
         </el-menu-item-group>
         <el-menu-item-group title="分组2">
-          <el-menu-item index="2-3">选项3</el-menu-item>
+          <el-menu-item index="3-3">选项3</el-menu-item>
         </el-menu-item-group>
-        <el-submenu index="2-4">
+        <el-submenu index="3-4">
           <template slot="title">选项4</template>
-          <el-menu-item index="2-4-1">选项1</el-menu-item>
+          <el-menu-item index="3-4-1">选项1</el-menu-item>
         </el-submenu>
       </el-submenu>
       <el-menu-item index="2">导航二</el-menu-item>
