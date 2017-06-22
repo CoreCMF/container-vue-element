834783cc43e07197482ea942d5008e6df4876822
bigrocs <bigrocs@qq.com>
2017-06-22 15:20:53 +0800

    add longin

---
+++ src/components/Login.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="">
+  login
+</div>
+</template>
+<script>
+  export default {
+    name: 'cve-login',
+    data() {
+      return {
+      }
+    },
+    methods: {
+
+    }
+  }
+</script>
+<style lang="scss">
+</style>
