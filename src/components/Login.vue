218ca2099422cab9edbddca0c2144a4ffeeea81a
bigrocs <bigrocs@qq.com>
2017-06-27 16:13:33 +0800

    设计login页面

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,8 @@
 <template>
-<div class="">
-  login
+<div class="login-body">
+  <div class="login-item">
+    <bve-index/>
+  </div>
 </div>
 </template>
 <script>
@@ -8,12 +10,44 @@
     name: 'cve-login',
     data() {
       return {
+        data: null,
       }
     },
+    computed: {
+      apiUrl() {
+        return this.$route.meta.apiUrl
+      }
+    },
+    created () {
+      // this.initData()
+    },
+    watch: {
+      // apiUrl: 'initData',
+    },
     methods: {
-
+      initData() {
+        let _this = this
+        let apiUrl = this.apiUrl
+        let thenFunction = function(Response) {
+          _this.data = Response.data
+          console.log(_this.data);
+        }
+        this.$store.dispatch('getData',{ apiUrl, thenFunction})
+      }
     }
   }
 </script>
 <style lang="scss">
+.login-body{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #20A0FF;
+  position:absolute;
+  width: 100%;
+  height:100%;
+}
+.login-item{
+  margin-bottom: 6.18%;
+}
 </style>
